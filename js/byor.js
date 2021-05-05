cdcdbe318a3dcdc89b39706bb5c468cb5726f5ac
yoshifuji <t.yoshifuji@me.com>
2021-05-05 19:04:40 +0900

    Step VII: Function Components

--- js/byor.js
+++ js/byor.js
@@ -81,7 +81,11 @@ function commitWork(fiber) {
     return;
   }
 
-  const domParent = fiber.parent.dom;
+  let domParentFiber = fiber.parent;
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
+  }
+  const domParent = domParentFiber.dom;
 
   if (fiber.effectTag === 'PLACEMENT' && fiber.dom !== null) {
     domParent.appendChild(fiber.dom);
@@ -92,11 +96,19 @@ function commitWork(fiber) {
       fiber.props,
     );
   } else if (fiber.effectTag === 'DELETION') {
-    domParent.removeChild(fiber.dom);
+    commitDeletion(fiber, domParent)
   }
 
   commitWork(fiber.child);
   commitWork(fiber.sibling);
+}
+
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom);
+  } else {
+    commitDeletion(fiber.child, domParent);
+  }
 }
 
 function render(element, container) {
@@ -160,13 +172,12 @@ function workLoop(deadline) {
 requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
+  const isFunctionComponent = fiber.type instanceof Function;
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
   }
-
-  const elements = fiber.props.children;
-
-  reconcileChildren(fiber, elements);
 
   if (fiber.child) {
     return fiber.child;
@@ -179,6 +190,19 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent;
   }
+}
+
+function updateFunctionComponent(fiber) {
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  reconcileChildren(fiber, fiber.props.children);
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -235,20 +259,16 @@ const Byor = {
   render,
 };
 
-const element = Byor.createElement(
-  'div',
-  { id: 'foo' },
-  Byor.createElement('a', null, 'bar'),
-  Byor.createElement('b'),
-);
+function App(props) {
+  return Byor.createElement(
+    'h1',
+    null,
+    'Hello, ',
+    props.name,
+  );
+}
 
-/** @jsx Byor.createElement */
-// const element = (
-//   <div id='foo'>
-//     <a>bar</a>
-//     <b />
-//   </div>
-// )
+const element = Byor.createElement(App, { name: 'foo' })
 
 const container = document.getElementById('root')
 Byor.render(element, container)
