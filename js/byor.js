c1eebf88bb3b555bc927576888e9e5df602031e2
yoshifuji <t.yoshifuji@me.com>
2021-05-05 20:34:10 +0900

    Step VIII: Hooks

--- js/byor.js
+++ js/byor.js
@@ -14,7 +14,7 @@ function createDom(fiber) {
   return dom;
 }
 
-const isEvent = (key) => key.startWith('on');
+const isEvent = (key) => key.startsWith('on');
 const isProperty = (key) => key !== 'children' && !isEvent(key);
 const isNew = (prev, next) => (key) => {
   return prev[key] !== next[key];
@@ -192,9 +192,47 @@ function performUnitOfWork(fiber) {
   }
 }
 
+let wipFiber = null;
+let hookIndex = null;
+
 function updateFunctionComponent(fiber) {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hooks = [];
+
   const children = [fiber.type(fiber.props)];
   reconcileChildren(fiber, children);
+}
+
+function useState(initial) {
+  const oldHook = wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex];
+
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  };
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach((action) => {
+    hook.state = action(hook.state);
+  })
+
+  const setState = (action) => {
+    hook.queue.push(action);
+
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    };
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  }
+
+  wipFiber.hooks.push(hook);
+  hookIndex++;
+
+  return [hook.state, setState];
 }
 
 function updateHostComponent(fiber) {
@@ -243,6 +281,10 @@ function reconcileChildren(wipFiber, elements) {
       deletions.push(oldFiber);
     }
 
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
     if (index === 0) {
       wipFiber.child = newFiber;
     } else {
@@ -257,18 +299,36 @@ function reconcileChildren(wipFiber, elements) {
 const Byor = {
   createElement,
   render,
+  useState,
 };
 
-function App(props) {
+function PlusButton(props) {
   return Byor.createElement(
-    'h1',
-    null,
-    'Hello, ',
-    props.name,
+    'button',
+    {
+      onclick: props.onClick,
+    },
+    'plus',
   );
 }
 
-const element = Byor.createElement(App, { name: 'foo' })
+function Counter() {
+  const [state, setState] = Byor.useState(1);
+
+  const onClick = () => {
+    setState(prevCount => prevCount + 1);
+  }
+
+  return Byor.createElement(
+    'h1',
+    null,
+    'Count: ',
+    state,
+    Byor.createElement(PlusButton, { onClick }),
+  );
+}
+
+const element = Byor.createElement(Counter);
 
 const container = document.getElementById('root')
 Byor.render(element, container)
